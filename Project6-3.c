4042912a663630b5b42194106c37d27e03f31357
Renshai <william.howard50@gmail.com>
2023-11-10 18:34:58 -0500

    Rough version of Project 6-3

---
+++ Project6-3.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <string.h>
+
+struct record
+{
+    char name[50];
+    char email[50];
+    char phone[50];
+};
+
+void display_menu();
+void list_record(const struct record rec[], int num);
+void view_record(const struct record rec[]);
+struct record add_record();
+
+int main(void)
+{
+    struct record list_o_records[10] = {0};
+    struct record p1 = {"Guido Van Rossum", "BDFL@pythn.org", "123-456-7890"};
+    struct record p2 = {"Eric Idle", "eric@ericidle.com", "+44 20 7946 0958"};
+    int num_records = 2;
+    int rec_no = 0;
+    char choice[5] = {0};
+    char list[] = "list";
+    char view[] = "view";
+    char add[] = "add";
+    char del[] = "del";
+    char exit[] = "exit";
+
+    list_o_records[0] = p1;
+    list_o_records[1] = p2;
+
+    printf("Contact Manager\n\n");
+
+    display_menu();
+
+    while (1)
+    {
+        printf("Enter a choice: ");
+        scanf("%s", choice);
+
+        if (strcmp(choice, list) == 0)
+        {
+            list_record(list_o_records, num_records);
+        }
+        else if (strcmp(choice, view) == 0)
+        {
+            view_record(list_o_records);
+        }
+        else if (strcmp(choice, add) == 0)
+        {
+            list_o_records[num_records++] = add_record();
+            printf("\n");
+        }
+        else if (strcmp(choice, del) == 0)
+        {
+            printf("Enter number: ");
+            scanf("%d", &rec_no);
+            printf("%s was deleted.\n\n", list_o_records[rec_no - 1].name);
+            memset(list_o_records[rec_no - 1].name, 0, sizeof(list_o_records[rec_no - 1].name));
+            memset(list_o_records[rec_no - 1].email, 0, sizeof(list_o_records[rec_no - 1].email));
+            memset(list_o_records[rec_no - 1].phone, 0, sizeof(list_o_records[rec_no - 1].phone));
+            num_records--;
+        }
+        else if (strcmp(choice, exit) == 0)
+        {
+            break;
+        }
+    }
+    return 0;
+}
+
+void display_menu()
+{
+    printf("COMMAND MENU\n");
+    printf("list - Display all contacts\n");
+    printf("view - View a contact\n");
+    printf("add - Add a contact\n");
+    printf("del - Delete a contact\n");
+    printf("exit - Exit program\n\n");
+}
+
+void list_record(const struct record rec[], int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        printf("Name: %s\n", rec[i].name);
+        printf("Email: %s\n", rec[i].email);
+        printf("Phone: %s\n\n", rec[i].phone);
+    }
+}
+
+struct record add_record()
+{
+    struct record p1 = {0};
+
+    printf("Enter name: ");
+    scanf("%s", p1.name);
+    printf("Enter email: ");
+    scanf("%s", p1.email);
+    printf("Enter phone number: ");
+    scanf("%s", p1.phone);
+
+    return p1;
+}
+
+void view_record(const struct record rec[])
+{
+    int num = 0;
+
+    printf("Enter record number: ");
+    scanf("%d", &num);
+
+    num = num - 1;
+
+    printf("\nName: %s\n", rec[num].name);
+    printf("Email: %s\n", rec[num].email);
+    printf("Phone: %s\n\n", rec[num].phone);
+}
